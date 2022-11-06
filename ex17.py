2f394f08d527c415f85928dee9cbd8858adb6cc2
Nilay Atabek <115468435+Nilayatabek@users.noreply.github.com>
2022-11-06 14:49:01 +0300

    Add files via upload

---
+++ ex17.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+if a!=0:
+    if a==c:
+        print("3 basamaklı sayınız", a,b,c, "palindromik")
+    else:
+         print("3 basamaklı sayınız palindromik değil ya da sayınız üç basamaklı değil")
+       
+    if a==d and b==c:
+        print("4 basamaklı sayınız", a,b,c,d, "palindromik")
+    else:
+            print(" 4 basamaklı sayınız palindromik değil")
+            
+            
