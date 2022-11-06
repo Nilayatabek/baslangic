f6dc4ad352ae4037de31ab77f1a55dab747528ae
Nilay Atabek <115468435+Nilayatabek@users.noreply.github.com>
2022-11-06 16:17:14 +0300

    Add files via upload

---
+++ ex10.py
@@ -0,0 +1,14 @@
+liste=[0,1,2,3,4,5,6,7,8,9]
+sayı=0
+for a in liste:
+     for b in liste:
+        for c in liste:
+            for d in liste:
+                for e in liste:
+                  if a!=0 and a==e and b==d:
+                   sayı+=1
+
+print(sayı)
+                    
+    
+                        
