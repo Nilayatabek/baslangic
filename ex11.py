5dd6b74f7560b4fbabffa46e4919e6a67e272515
Nilay Atabek <115468435+Nilayatabek@users.noreply.github.com>
2022-11-10 16:59:51 +0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,13 @@
+liste=[125,200,350]
+nile=[]
+for a in range(1,350):
+ for X in range(1,350):
+    if 125%X==200%X==350%X==a:
+        nile+=[a]
+
+print(max(nile))
+        
+        
+
+     
+    
